6622ee19a6ac1bdfd6481ac88b608d902c7ff208
Jan Koudela <jan@koudela.eu>
2023-05-02 17:07:47 -0600

    Added SAS data object

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -30,3 +30,56 @@ resource "massdriver_artifact" "azure_storage_account_blob" {
     }
   )
 }
+
+
+data "azurerm_storage_account_sas" "sas" {
+  connection_string = module.azure_storage_account.primary_connection_string
+  https_only        = true
+  start             = timestamp()
+  expiry            = timeadd(timestamp(), "2160h") # 90 days
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = true
+    process = true
+    tag     = false
+    filter  = false
+  }
+
+}
+
+resource "massdriver_artifact" "azure_storage_account_blob_sas" {
+  field                = "azure_storage_account_blob_sas"
+  provider_resource_id = module.azure_storage_account.account_id
+  name                 = "Azure Blob Storage Account ${var.md_metadata.name_prefix} (${module.azure_storage_account.account_id}) SAS"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = {
+          ari      = module.azure_storage_account.account_id
+          endpoint = module.azure_storage_account.primary_blob_endpoint
+        }
+      }
+      specs = {
+        azure = {
+          sas = data.azurerm_storage_account_sas.sas.sas
+        }
+      }
+    }
+  )
+}
